925fb81c5864ac5d56b8384eaa33b2c3fca11307
navaneeth <navaneethbhasuram@gmail.com>
2022-02-05 21:37:49 +0530

    comments and reply display added

--- resources/views/admin/posts/partials/comments.blade.php
+++ resources/views/admin/posts/partials/comments.blade.php
@@ -1,3 +1,6 @@
+<div style="margin-left: 1.5rem">
 @foreach($data as $comment)
-    <p>{{$comment->$comment}}</p>
+    <p>{{$comment->comment}}</p>
+    @include('admin.posts.partials.comments',['data'=>$comment->replies])
 @endforeach
+</div>
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts.layout')
 @section('content')
-<h3>Blog Post</h3>
-<div class="row">
-    <div class="col-12">
-        {{$post->title}}
+    <h3>Blog Post</h3>
+    <div class="row">
+        <div class="col-2">
+            Title:
+        </div>
+        <div class="col-10">
+           {{$post->title}}
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-12">
-        {{$post->body}}
+    <div class="row">
+        <div class="col-2">
+            Body:
+        </div>
+        <div class="col-10">
+            {{$post->body}}
+        </div>
     </div>
-</div>
+    <h4>Comments</h4>
+    @forelse($post->comments as $comment)
+        <p>{{$comment->comment}}</p>
+        @include('admin.posts.partials.comments',['data'=>$comment->replies])
+    @empty
+        <p class="text-muted">No comments yet</p>
+    @endforelse
 @endsection
